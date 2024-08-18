02c849a9f2e0d0d71a0b8a7f84464b2d16cf4902
mansachi <amadichinda1001@outlook.com>
2024-08-18 23:52:55 +0100

    Updated models

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -13,9 +13,18 @@ class Book(models.Model):
 
     title = models.CharField(max_length=250)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    publication_year = models.IntegerField()
+
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add book") 
+            ("can_change_book", "Can change book"),
+            ("can_delete_book", "Can delete book"),
+         ]
 
     def __str__(self):
         return self.title
+                                                                    ]
 
 class Library(models.Model):
 
@@ -31,3 +40,32 @@ class Librarian(models.Model):
 
     def __str__(self):
         return self.name
+
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.models import User
+
+
+class UserProfile(models.Model):
+    ROLE_CHOICES = [
+        ("Admin", "Admin"),
+        ("Librarian", "Librarian"),
+        ("Member", "Member"),
+    ]
+                                            ]
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=50, choices=ROLE_CHOICES)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.userprofile.save()
