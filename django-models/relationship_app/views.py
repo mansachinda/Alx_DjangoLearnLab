1871963c345c97aa6e35e5d358a997972303720c
mansachi <amadichinda1001@outlook.com>
2024-08-18 22:51:16 +0100

    Updated VIEWS.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,3 +1,24 @@
+from typing import Any
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic.detail import DetailView
+from .models import Book
+from .models import Library
 
-# Create your views here.
+# This is where we Create our views.
+
+def list_books(request):
+    items = Book.objects.all()
+    book_list = [f"{i.title} by {i.author}" for i in items]
+    return render(request, "relationship_app/list_books.html", {"book_list": book_list})
+
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
+
+    def get_context_data(self, **kwargs: Any):
+        context = super().get_context_data(**kwargs)
+        context['library'] = self.object
+        return context
